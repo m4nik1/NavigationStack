240277593c913b3bd58d9dd4ab452213b9657330
m4nik1 <maniksoomro@hotmail.com>
2022-03-06 16:03:01 +0000

    First

---
+++ src/tf_robot_broadcaster.py
@@ -0,0 +1,23 @@
+import roslib
+import rospy
+
+import tf
+import turtlesim.msg
+
+def handle_turtle_pose(msg, turtlename):
+    br = tf.TransformBroadcaster()
+    br.sendTransform((msg.x, msg.y, 0),
+                     tf.transformations.quaternion_from_euler(0, 0, msg.theta),
+                     rospy.Time.now(),
+                     turtlename,
+                     "world")
+
+if __name__ == "__main__":
+    rospy.init_node("turtle_tf_broadcaster")
+    turtlename = rospy.get_param("~turtle")
+    rospy.Subscribe('/%s/pose' % turtlename,
+                    turtlesim.msg.pose,
+                    handle_turtle_pose,
+                    turtlename)
+    rospy.spin()
+                     
